4dd89060c681dc69b6f079c9422f669d9a8ffc35
Ngo Thao Huong <100118053+nghinn1012@users.noreply.github.com>
2024-03-24 20:54:10 +0700

    thêm testcase tuần 5

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -7,28 +7,53 @@ import static org.junit.Assert.assertEquals;
 
 public class MainTest {
     MainProgrammer main = new MainProgrammer();
+//    @Test
+//    public void w4_test1() {
+//        assertEquals("Input không hợp lệ", MainProgrammer.evaluateProgrammer(13, false));
+//    }
+//    @Test
+//    public void w4_test2() {
+//        assertEquals("Senior", MainProgrammer.evaluateProgrammer(8, true));
+//    }
+//    @Test
+//    public void w4_test3() {
+//        assertEquals("Junior", MainProgrammer.evaluateProgrammer(5, true));
+//    }
+//    @Test
+//    public void w4_test4() {
+//        assertEquals("Chưa rõ xếp loại", MainProgrammer.evaluateProgrammer(2, true));
+//    }
+//    @Test
+//    public void w4_test5() {
+//        assertEquals("Intern", MainProgrammer.evaluateProgrammer(1, false));
+//    }
+//    @Test
+//    public void w4_test6() {
+//        assertEquals("Chưa rõ xếp loại", MainProgrammer.evaluateProgrammer(5, false));
+//    }
     @Test
-    public void test1() {
-        assertEquals("Input không hợp lệ", MainProgrammer.evaluateProgrammer(13, false));
+    public void w5_test1() {
+        assertEquals("Input không hợp lệ", MainProgrammer.evaluateProgrammer(12, false));
     }
     @Test
-    public void test2() {
-        assertEquals("Senior", MainProgrammer.evaluateProgrammer(8, true));
+    public void w5_test2() {
+        assertEquals("Senior", MainProgrammer.evaluateProgrammer(9, true));
     }
     @Test
-    public void test3() {
+    public void w5_test3() {
         assertEquals("Junior", MainProgrammer.evaluateProgrammer(5, true));
     }
     @Test
-    public void test4() {
+    public void w5_test4() {
         assertEquals("Chưa rõ xếp loại", MainProgrammer.evaluateProgrammer(2, true));
     }
     @Test
-    public void test5() {
-        assertEquals("Intern", MainProgrammer.evaluateProgrammer(1, false));
-    }
-    @Test
-    public void test6() {
+    public void w5_test5() {
         assertEquals("Chưa rõ xếp loại", MainProgrammer.evaluateProgrammer(5, false));
     }
+    @Test
+    public void w5_test6() {
+        assertEquals("Intern", MainProgrammer.evaluateProgrammer(1, false));
+    }
+
 }
